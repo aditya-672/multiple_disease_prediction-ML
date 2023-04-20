95fe0073acab200e48e8fd501501361ab55cbc42
aditya-672 <waghmareaditya08@gmail.com>
2023-04-21 00:40:23 +0530

    Third Commit

--- mdp.py
+++ mdp.py
@@ -12,11 +12,11 @@ from streamlit_option_menu import option_menu
 
 # loading the saved models
 
-diabetes_model = pickle.load(open("D:/OneDrive - Vidyalankar Institute of Technology/VIT/SEM 6/MiniProject_Sem6/diabetes_model.sav", 'rb'))
+diabetes_model = pickle.load(open('diabetes_model.sav', 'rb'))
 
-heart_disease_model = pickle.load(open("D:/OneDrive - Vidyalankar Institute of Technology/VIT/SEM 6/MiniProject_Sem6/heart_disease_model.sav",'rb'))
+heart_disease_model = pickle.load(open('heart_disease_model.sav','rb'))
 
-parkinsons_model = pickle.load(open("D:/OneDrive - Vidyalankar Institute of Technology/VIT/SEM 6/MiniProject_Sem6/parkinsons_model.sav", 'rb'))
+parkinsons_model = pickle.load(open('parkinsons_model.sav', 'rb'))
 
 
 
